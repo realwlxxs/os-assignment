ce076a0518df5d1e9d760e7e941ce2d0b634acab
Zhengyang Lu <realwlxxs@163.com>
2023-05-27 14:04:31 +0800

    feat: mq

--- client.cc
+++ client.cc
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <sys/ipc.h>
+#include <sys/msg.h>
 #include <sys/shm.h>
 
 #define BUFFER_SIZE 1024
@@ -16,9 +17,25 @@ void ipc_shm() {
   printf("%s\n", shmaddr);
 }
 
+struct msg {
+  long type;
+  char text[BUFFER_SIZE];
+};
+
+void ipc_mq() {
+  key_t key = ftok("./ipcid", 2);
+  int msgid = msgget(key, IPC_CREAT | 0666);
+  msg m;
+  m.type = 1;
+  msgrcv(msgid, &m, sizeof(m) - sizeof(m.type), m.type, 0);
+  printf("%s\n", m.text);
+}
+
 int main(int argc, char *argv[]) {
   assert(argc == 2);
   if (!strcmp(argv[1], "shm")) {
     ipc_shm();
+  } else if (!strcmp(argv[1], "mq")) {
+    ipc_mq();
   }
 }
--- server.cc
+++ server.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <sys/ipc.h>
+#include <sys/msg.h>
 #include <sys/shm.h>
 
 #define BUFFER_SIZE 1024
@@ -19,9 +20,28 @@ void ipc_shm() {
   }
 }
 
+struct msg {
+  long type;
+  char text[BUFFER_SIZE];
+};
+
+void ipc_mq() {
+  key_t key = ftok("./ipcid", 2);
+  int msgid = msgget(key, IPC_CREAT | 0666);
+  msg m;
+  m.type = 1;
+  string s;
+  while (getline(cin, s)) {
+    strcpy(m.text, s.c_str());
+    msgsnd(msgid, &m, sizeof(m) - sizeof(m.type), 0);
+  }
+}
+
 int main(int argc, char *argv[]) {
   assert(argc == 2);
   if (!strcmp(argv[1], "shm")) {
     ipc_shm();
+  } else if (!strcmp(argv[1], "mq")) {
+    ipc_mq();
   }
 }
